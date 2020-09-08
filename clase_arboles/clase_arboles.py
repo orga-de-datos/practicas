b469d21ab08df71519602ccaee9eb64daedc803f
SDonikian <santiago.donikian@moni.com.ar>
2020-09-07 22:50:34 -0300

    cambio dataset a insurance

--- clase_arboles/clase_arboles.py
+++ clase_arboles/clase_arboles.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.5.1
+#       jupytext_version: 1.6.0
 #   kernelspec:
-#     display_name: Python 3 (venv)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -27,16 +27,48 @@ import numpy as np
 import pandas as pd
 from IPython.display import SVG, display
 from ipywidgets import Button, IntSlider, interactive
-from sklearn import tree
-from sklearn.datasets import load_iris
+from sklearn import preprocessing, tree
+from sklearn.preprocessing import OneHotEncoder
 
 # # Carga de datos
-# Vamos a usar el [iris dataset](http://archive.ics.uci.edu/ml/datasets/Iris/), cargandolo directamente con [un helper de sklearn](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html). Notar que devuelve un [bunch](https://scikit-learn.org/stable/modules/generated/sklearn.utils.Bunch.html#sklearn.utils.Bunch), en lugar de un dataframe.
+# Vamos a usar un dataset de [valor de seguros](https://www.kaggle.com/mirichoi0218/insurance), cargandolo desde [github](https://github.com/stedy/Machine-Learning-with-R-datasets).
 
-iris = load_iris(return_X_y=False)
-X = iris.data
-y = iris.target
+# +
+@lru_cache()
+def get_data():
+    # leemos el dataset a explorar
+    return pd.read_csv(
+        'https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv'
+    )
 
+
+dataset = get_data()
+# -
+
+dataset.info()
+
+dataset.head()
+
+
+# # Manejo de variables
+
+# +
+def fe(df):
+    dummies = pd.get_dummies(df[['sex', 'region']], drop_first=True)
+    df = df.drop(columns=['sex', 'region']).join(dummies)
+
+    X = df.drop(columns=['smoker'])
+    label_encoder = preprocessing.LabelEncoder()
+    label_encoder.fit(df.smoker)
+    y = label_encoder.transform(df.smoker)
+    return X, y, df, label_encoder
+
+
+X, y, df, y_encoder = fe(dataset)
+df.head()
+
+
+# -
 
 # # Entrenamiento
 # Sklearn [propone](https://scikit-learn.org/stable/developers/develop.html) una interfaz común a todos sus estimadores, pero sin enforzarla. Se espera que tengan un método `.fit` y un método `.predict`. Toda la inicialización de hiperparametros debe estar en el `__init__` de la clase, y deben tener valores por defecto.
@@ -44,25 +76,25 @@ y = iris.target
 # Vamos a entrenar un modelo y jugar con la profundidad máxima del árbol. El árbol entrenado, lo visualizaremos utilizando código similar al de la documentación de sklearn encontrado [aquí](https://scikit-learn.org/stable/modules/tree.html#classification).
 
 # +
-@lru_cache()
-def get_iris_data():
-    iris = load_iris(return_X_y=False)
-    X = iris.data
-    y = iris.target
-    return X, y, iris
+def get_tree(X, y, max_depth=5):
+    clf = tree.DecisionTreeClassifier(
+        random_state=117, max_depth=max_depth, min_samples_leaf=10
+    )
+    clf.fit(X, y)
+    return clf
 
 
 def plot_tree(max_depth):
 
-    X, y, iris = get_iris_data()
+    df = get_data()
+    X, y, df, y_encoder = fe(df)
+    clf = get_tree(X, y, max_depth)
 
-    clf = tree.DecisionTreeClassifier(random_state=117, max_depth=max_depth)
-    clf.fit(X, y)
     dot_data = tree.export_graphviz(
         clf,
         out_file=None,
-        feature_names=iris.feature_names,
-        class_names=iris.target_names,
+        feature_names=X.columns,
+        class_names=list(y_encoder.classes_),
         filled=True,
         rounded=True,
         special_characters=True,
@@ -75,25 +107,20 @@ inter = interactive(plot_tree, max_depth=IntSlider(min=1, max=15))
 display(inter)
 # -
 
-# Notar que el árbol no crece a una profundida mayor a 5 aún cuando el hiperparámetro `max_depth` lo permite.
-
 # # Explorando las particiones
 #
 # [dtreeviz](https://github.com/parrt/dtreeviz) permite explorar un poco mas los árboles. Es un poco más visual que su contraparte de sklearn.
 
-X, y, iris = get_iris_data()
-
 # +
-clf = tree.DecisionTreeClassifier(random_state=117, max_depth=5)
-clf.fit(X, y)
+clf = get_tree(X, y)
 
 viz = dtreeviz.dtreeviz(
     clf,
-    iris.data,
-    iris.target,
-    target_name='variety',
-    feature_names=iris.feature_names,
-    class_names=iris.target_names.tolist(),
+    X,
+    y,
+    target_name='smoker',
+    feature_names=list(X.columns),
+    class_names=list(y_encoder.classes_),
     scale=2.0,
 )
 
@@ -104,28 +131,24 @@ display(viz)
 #
 # Si tomamos una muestra al azar de nuestro dataset y le pedimos al árbol una predicción, como se hace?
 
-iris_df = pd.DataFrame(
-    data=np.c_[iris.data, pd.Series(iris.target_names.tolist())[iris.target]],
-    columns=iris.feature_names + ['variety'],
-)
-iris_df.sample(5)
+df.sample(5)
 
 
 # +
 def explore_prediction():
-    X, y, iris = get_iris_data()
-    x_sample = iris_df.sample()
+    X, y, df, y_encoder = fe(get_data())
+    x_sample = df.sample()
     display(x_sample)
 
     viz = dtreeviz.dtreeviz(
         clf,
-        iris.data,
-        iris.target,
-        target_name='variety',
-        feature_names=iris.feature_names,
-        class_names=iris.target_names.tolist(),
+        X,
+        y,
+        target_name='smoker',
+        feature_names=list(X.columns),
+        class_names=list(y_encoder.classes_),
         scale=1.5,
-        X=x_sample[iris.feature_names].iloc[0].values,
+        X=x_sample[X.columns].iloc[0].values,
     )
 
     display(viz)
