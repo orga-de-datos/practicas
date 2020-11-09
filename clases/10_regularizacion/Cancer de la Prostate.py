d006a245148739edf1af82084be388ae93130944
juanma <jmanuoz90@gmail.com>
2020-11-09 17:41:51 -0300

    practicas regularizacion

---
+++ clases/10_regularizacion/Cancer de la Prostate.py
@@ -0,0 +1,257 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Cancer de próstata
+#
+#
+# Cargando bibliotecas y datos
+
+# coding: utf-8
+# %matplotlib inline  
+import matplotlib.pyplot as plt
+plt.rcParams['font.size'] = 14
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+from sklearn.linear_model import Ridge, RidgeCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import ElasticNet
+import seaborn as sns
+from sklearn import preprocessing
+import warnings
+from sklearn.model_selection import cross_val_score
+warnings.filterwarnings(action='ignore')
+
+raw_data = pd.read_csv('https://drive.google.com/file/d/1BEi_-ZifLqIkDGD8q0Uti7xPFynFcyJ0/view?usp=sharing', delimiter='\t')
+
+raw_data.head(10)
+
+raw_data.shape
+
+# Este es un conjunto de datos bastante estándar, relativamente pequeño, con 97 observaciones y 9 variables. Aquí definiremos como objetivo y la cantidad de expresión de antígeno que está asociada con la detección de este cáncer (la columna lpsa del conjunto de datos). Las otras variables son características asociadas.
+#
+# Recuperamos nuestras variables explicativas para la regresión, que colocamos en una matriz X y nuestra variable explicada Y por separado. No recuperamos la última columna del conjunto de datos, que es un valor booleano asociado con la presencia de cáncer, porque no tratamos las variables discretas en este caso.
+
+X_train = raw_data[['lweight','age','lbph','svi','lcp','gleason','pgg45']]
+y_train = raw_data[['lpsa']]
+
+
+def rmse_cv(model, X_train, y_train):
+    rmse = np.sqrt(
+        -cross_val_score(
+            model, X_train, y_train, scoring="neg_mean_squared_error", cv=5
+        )
+    )
+    return rmse.mean()
+
+
+# # Baseline : regresión lineal clásica
+
+# +
+# Recuperamos el MSE en el conjunto de datos de prueba como baseline del error
+baseline_error = rmse_cv(LinearRegression(), X_train, y_train)
+
+print(baseline_error)
+# -
+
+# # Ridge Regresssion
+#
+# Antes que nada, escalamos los datos
+
+plt.figure(figsize=[12, 8])
+plt.xlabel('Variables', fontsize=20)
+sns.set_theme(style="whitegrid")
+sns.boxplot(data = raw_data[['lweight','age','lbph','svi','lcp','gleason','pgg45']])
+
+scaler = preprocessing.StandardScaler()
+# Fit your data on the scaler object
+to_scale = raw_data.iloc[:,1:10]
+scaler.fit(to_scale)
+scaled_df = scaler.transform(to_scale)
+names = to_scale.columns
+scaled_prostate_df = pd.DataFrame(scaled_df, columns=names)
+scaled_prostate_df['train'] = raw_data.iloc[:,10]
+
+plt.figure(figsize=[12, 8])
+plt.xlabel('Variables', fontsize=20)
+sns.set_theme(style="whitegrid")
+sns.boxplot(data = scaled_prostate_df[['lweight','age','lbph','svi','lcp','gleason','pgg45']])
+
+X_train = scaled_prostate_df.iloc[:,:][['lweight','age','lbph','svi','lcp','gleason','pgg45']]
+y_train = raw_data.iloc[:]['lpsa']
+
+
+# Llamamos alfa al coeficiente de regularización. 
+# Vamos a probar varios de ellos para determinar cuál es el más óptimo.
+
+n_alphas = 200
+alphas = np.logspace(-5, 5, n_alphas)
+
+# Ahora podemos probar todas las regresiones estriadas con los diferentes valores del hiperparámetro α. Recuperamos las ponderaciones de los distintos coeficientes de la regresión asociada así como el error cuadrático.
+
+# +
+ridge = linear_model.Ridge()
+
+coefs = []
+errors = []
+for a in alphas:
+    ridge.set_params(alpha=a)
+    ridge.fit(X_train, y_train)
+    coefs.append(ridge.coef_)
+    errors.append([baseline_error, rmse_cv(Ridge(alpha=a), X_train, y_train)])
+# -
+
+# Puede visualizar la evolución del valor de los diferentes pesos asociados a los parámetros:
+
+# +
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('pesos', fontsize=20)
+plt.title('Coeficientes de la regresión Ridge en función del alpha', fontsize=26)
+plt.axis('tight')
+plt.show()
+# -
+
+# El valor de alfa disminuye los pesos de todos los parámetros de la regresión. Ahora estudiemos el valor del error cuadrático:
+
+# +
+
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+
+[baseError,RidgeError] = ax.plot(alphas, errors)
+ax.set_xscale('log')
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('error', fontsize=20)
+plt.axis('tight')
+leg2 = ax.legend([baseError,RidgeError],['Baseline','Ridge'], loc='lower right')
+ax.add_artist(leg2)
+plt.show()
+# -
+
+# Como podemos ver, la regularización reduce el error en el conjunto de datos de prueba. Hacia alfa = 10, el mínimo parece encontrarse para la regresión Ridge. Podemos recuperar el valor mínimo:
+
+min(errors)
+
+# El primer valor corresponde al error con regresión lineal clásica y el segundo valor corresponde al error con regresión Ridge.
+
+# # Regresión Lasso
+
+# También probamos distintos valores de alpha Lasso
+
+# +
+n_alphas = 300
+alphas = np.logspace(-5, 1, n_alphas)
+lasso = linear_model.Lasso(fit_intercept=False)
+
+coefs = []
+errors = []
+for a in alphas:
+    lasso.set_params(alpha=a)
+    lasso.fit(X_train, y_train)
+    coefs.append(lasso.coef_)
+    errors.append([baseline_error,rmse_cv(linear_model.Lasso(alpha=a), X_train, y_train)])
+
+# +
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('weights', fontsize=20)
+plt.axis('tight')
+
+plt.show()
+# -
+
+# Como podemos ver, Lasso te permite eliminar variables estableciendo su peso en cero. Este es el caso si dos variables están correlacionadas. Uno será seleccionado por Lasso y el otro borrado. Esta es también su ventaja sobre una regresión Ridge que no seleccionará variables.
+#
+# Ahora podemos observar el comportamiento del error.
+
+# +
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+[baseError,LassoError] = ax.plot(alphas, errors)
+
+ax.set_xscale('log')
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('error', fontsize=20)
+plt.axis('tight')
+leg2 = ax.legend([baseError,LassoError],['Baseline','Lasso'], loc='lower right')
+ax.add_artist(leg2)
+plt.show()
+# -
+
+min(errors)
+
+# En este caso Ridge funcionó mejor!
+#
+# Veamos ahora ElasticNet
+
+# # Elastic Net
+
+# +
+n_alphas = 300
+alphas = np.logspace(-5, 1, n_alphas)
+
+
+coefs = []
+errors = []
+for a in alphas:
+    elasticNet = ElasticNet(alpha=a, l1_ratio=0.5)
+    elasticNet.fit(X_train, y_train)
+    coefs.append(elasticNet.coef_)
+    errors.append([baseline_error,rmse_cv(ElasticNet(alpha=a, l1_ratio=0.5), X_train, y_train)])
+
+# +
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+
+ax.plot(alphas, coefs)
+ax.set_xscale('log')
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('weights', fontsize=20)
+plt.axis('tight')
+plt.show()
+# -
+
+# ¿Por qué Elastic Net también a la larga anula (aunque mas tarde) los coeficientes?
+
+# +
+plt.figure(figsize=[12, 8])
+ax = plt.gca()
+[baseError,ElasticNetError] = ax.plot(alphas, errors)
+
+ax.set_xscale('log')
+plt.xlabel('alpha', fontsize=20)
+plt.ylabel('error', fontsize=20)
+plt.axis('tight')
+leg2 = ax.legend([baseError,ElasticNetError],['Baseline','ElasticNet'], loc='lower right')
+ax.add_artist(leg2)
+plt.show()
+# -
+
+min(errors)
+
+# Si bien Elastic Net mejora respecto a la regresión clásica, la competencia la ganó Ridge!
+#
+# ¿Por qué puede ser esto?
---
+++ clases/10_regularizacion/Regularización.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ### Regularización
+#
+# Utilizaremos el dataset "Boston" que nos provee sklearn, el mismo contiene valores de propiedades en el estado de Boston.
+
+# +
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn.linear_model import LassoCV
+
+import warnings
+warnings.filterwarnings(action='ignore')
+# -
+
+boston = load_boston()
+boston_df = pd.DataFrame(boston.data, columns=boston.feature_names)
+
+boston_df['Price'] = boston.target
+boston_df.head()
+
+boston_df.info()
+
+boston_df.describe()
+
+# ### Semántica de los datos
+#
+# Cada registro de la base de datos describe un suburbio o una ciudad de Boston. Los datos se obtuvieron del Área Estadística Metropolitana Estándar de Boston (SMSA) en 1970. Los atributos se definen de la siguiente manera (tomados del Repositorio de Aprendizaje Automático de UCI1):
+#
+#  - CRIM: tasa de delincuencia per cápita
+#  - ZN: proporción de terreno residencial dividido en zonas para lotes de más de 25,000 pies cuadrados.
+#  - INDUS: proporción de héctareas comerciales no minoristas por ciudad
+#  - CHAS: Variable ficticia de Charles River (= 1 si el tramo limita con el río; 0 en caso contrario)
+#  - NOX: concentración de óxidos nítricos (partes por 10 millones)
+#  - RM: número medio de habitaciones por vivienda
+#  - AGE: proporción de unidades ocupadas por sus propietarios construidas antes de 1940
+#  - DIS: distancias ponderadas a cinco centros de empleo de Boston
+#  - RAD: índice de accesibilidad a carreteras radiales
+#  - TAX: tasa de impuesto a la propiedad de valor total por  10,000
+#  - PTRATIO: proporción alumno-maestro por ciudad
+#  - B: proporción de gente negra
+#  - LSTAT:% de menor estatus de la población
+#  - MEDV: Valor medio de las viviendas ocupadas por sus propietarios en 1000
+#
+# #### Observaciones
+#
+#  - Podemos ver en quién armó los datos una clara discriminación racial, incluyen como dato potencialmente relevante para el valor de la propiedad, la proporción de gente negra. Mas allá de si esto funciona bien como predictor nos va abriendo las puertas para uno de los temas que veremos hacia el final de la materia: Ética de la inteligencia artificial
+#  - Podemos ver que los atributos de entrada tienen una mezcla de unidades.
+#
+# Veamos ahora como es la correlación de los features de este dataset
+
+# +
+
+corrmat = boston_df.corr()
+k = 12  # number of variables for heatmap
+cols = corrmat.nlargest(k, 'Price')['Price'].index
+cm = np.corrcoef(boston_df[cols].values.T)
+sns.set(font_scale=1.25)
+fig, ax = plt.subplots(figsize=(15, 15))
+hm = sns.heatmap(
+    cm,
+    cbar=True,
+    annot=True,
+    square=True,
+    fmt='.2f',
+    annot_kws={'size': 10},
+    yticklabels=cols.values,
+    xticklabels=cols.values,
+)
+
+plt.show()
+# -
+
+# ### Regresion OLS
+#
+# Separemos nuestros datos en las variables independientes (X) y dependiente (y)
+
+# Comenzamos por hacer una regresión lineal para ver cual sería la performance sin regularización y que valor le asigna a cada feature. Para tener una buena estimación lo haremos mediante cross validation con un k-fold = 5
+
+# +
+boston_df['Price'] = boston.target
+X_train = boston_df.drop("Price", axis=1)
+y_train = boston_df.filter(items=["Price"])
+
+linreg = LinearRegression()
+rmse = np.sqrt(
+    -cross_val_score(
+        LinearRegression(), X_train, y_train, scoring="neg_mean_squared_error", cv=5
+    )
+)
+rmse.mean()
+# -
+
+linreg = LinearRegression()
+linreg.fit(X_train, y_train)
+linreg.coef_[0]
+
+# +
+
+coeficientes = pd.DataFrame(
+    {'Feature': X_train.columns, 'Beta': linreg.coef_[0]}, columns=['Feature', 'Beta']
+)
+coeficientes.sort_values(by=['Beta'])
+# -
+
+coef = pd.Series(linreg.coef_[0], index=X_train.columns)
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Ridge ")
+
+
+# ### Ridge
+# Ahora procederemos a escalar los datos mediante el cálculo visto en la teórica
+
+# +
+
+scaler = preprocessing.StandardScaler()
+# Fit your data on the scaler object
+
+scaler.fit(boston_df)
+scaled_df = scaler.transform(boston_df)
+names = boston_df.columns
+scaled_boston_df = pd.DataFrame(scaled_df, columns=names)
+
+
+# -
+
+X_train = scaled_boston_df.drop("Price", axis=1)
+y_train = scaled_boston_df.filter(items=["Price"])
+
+
+scaled_boston_df
+
+X_train = scaled_boston_df.drop("Price", axis=1)
+y_train = boston_df.filter(items=["Price"])
+
+
+# Ahora definiremos una nueva función para calcular el RMSE de un modelo mediante cross validation con un k-fold de 5
+
+
+def rmse_cv(model, X_train, y_train):
+    rmse = np.sqrt(
+        -cross_val_score(
+            model, X_train, y_train, scoring="neg_mean_squared_error", cv=5
+        )
+    )
+    return rmse
+
+
+# Volvemos a hacer split de la data poque ahor queremos trabajar con nuestro datos escalados
+
+# Comenzaremos por ajustar nuestra regresión mediante Ridge, probaremos con distintos valores de alpha y luego graficaremos el error (RMSE) en función de alpha
+
+model_ridge = Ridge()
+alphas = [
+    0.001,
+    0.005,
+    0.01,
+    0.05,
+    0.1,
+    0.3,
+    1,
+    3,
+    5,
+    10,
+    30,
+    50,
+    55,
+    75,
+    100,
+    120,
+    150,
+]
+cv_ridge = [rmse_cv(Ridge(alpha=alpha), X_train, y_train).mean() for alpha in alphas]
+
+cv_ridge = pd.Series(cv_ridge, index=alphas)
+cv_ridge.plot(title="Validation - Just Do It")
+plt.xlabel("alpha")
+plt.ylabel("rmse")
+
+cv_ridge.min()
+
+# Podemos ver que el error alcanza un mínimo de 5.4 con un alpha alrrededor de 80, mas precisamente 75 según el array de alphas que cargamos
+
+# +
+ridge = Ridge(alpha=75)
+ridge.fit(X_train, y_train)
+
+
+coeficientes = pd.DataFrame(
+    {'Feature': X_train.columns, 'Beta': ridge.coef_[0]}, columns=['Feature', 'Beta']
+)
+coeficientes.sort_values(by=['Beta'])
+# -
+
+coef = pd.Series(ridge.coef_[0], index=X_train.columns)
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Ridge ")
+
+# ### Lasso
+#
+# Veamos ahora como se ajusta mediante Lasso
+
+model_lasso = LassoCV(
+    alphas=[
+        10,
+        30,
+        50,
+        55,
+        75,
+        100,
+        120,
+        150,
+        5,
+        4,
+        3,
+        2,
+        1,
+        0.1,
+        0.01,
+        0.05,
+        0.005,
+        0.0005,
+        0.0001,
+    ],
+    cv=5,
+).fit(X_train, y_train.values.ravel())
+
+model_lasso.score( X_train, y_train.values.ravel())
+
+# Dado que Lasso tiende a anular totalmente algunos coeficientes, podemos ver cuantas y cuales de nuestros coeficientes fueron anulados y por lo tanto, cuales variables pueden ser prescindibles para este ajuste
+
+coef = pd.Series(model_lasso.coef_, index=X_train.columns)
+print(
+    "Lasso seleccionó "
+    + str(sum(coef != 0))
+    + " variables y eliminó las otras "
+    + str(sum(coef == 0))
+    + " variables"
+)
+
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Lasso ")
+
+# Que sucede si artificialmente creamos una nueva columna correlacionada linealmente con otra?
+
+X_train['new_col'] = X_train['RM'] * 2
+
+model_lasso = LassoCV(
+    alphas=[
+        10,
+        30,
+        50,
+        55,
+        75,
+        100,
+        120,
+        150,
+        5,
+        4,
+        3,
+        2,
+        1,
+        0.1,
+        0.01,
+        0.05,
+        0.005,
+        0.0005,
+        0.0001,
+    ],
+    cv=5,
+).fit(X_train, y_train.values.ravel())
+rmse_cv(model_lasso, X_train, y_train.values.ravel()).mean()
+
+coef = pd.Series(model_lasso.coef_, index=X_train.columns)
+print(
+    "Lasso seleccionó "
+    + str(sum(coef != 0))
+    + " variables y eliminó las otras "
+    + str(sum(coef == 0))
+    + " variables"
+)
+
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Lasso ")
+
+# Podemos ver que ahora elimina 3 variables, dado que la nueva que generamos esta correlacionada con la RM.
+#
+# También podemos ver que en vez de eliminar nuestra nueva  variable elimina la original.....
+#
+# ¿Porque podrá ser esto?
+
+# ### Elastic Net
+#
+# Veamos ahora como ajusta Elastic Net
+
+# +
+alphas = [0.001, 0.005, 0.01, 0.05, 0.1, 0.3, 1, 3, 5]
+
+cv_elasticNet = [
+    rmse_cv(ElasticNet(alpha=alpha, l1_ratio=0.5), X_train, y_train).mean()
+    for alpha in alphas
+]
+# -
+
+cv_elasticNet
+
+cv_elasticNet = pd.Series(cv_elasticNet, index=alphas)
+cv_elasticNet.plot(title="Validation - Just Do It")
+plt.xlabel("alpha")
+plt.ylabel("rmse")
+
+cv_elasticNet.min()
+
+# +
+elastic = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic.fit(X_train, y_train)
+
+coeficientes = pd.DataFrame(
+    {'Feature': X_train.columns, 'Beta': elastic.coef_}, columns=['Feature', 'Beta']
+)
+coeficientes.sort_values(by=['Beta'])
+# -
+
+coef = pd.Series(elastic.coef_, index=X_train.columns)
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Ridge ")
+
+# Veamos que pasa si al l1_ratio lo llevamos a 1
+
+# +
+elastic = ElasticNet(alpha=0.1, l1_ratio=1)
+elastic.fit(X_train, y_train)
+
+coeficientes = pd.DataFrame(
+    {'Feature': X_train.columns, 'Beta': elastic.coef_}, columns=['Feature', 'Beta']
+)
+coeficientes.sort_values(by=['Beta'])
+# -
+
+coef = pd.Series(elastic.coef_, index=X_train.columns)
+imp_coef = pd.concat([coef.sort_values()])
+plt.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind="barh")
+plt.title("Coeficientes en el Modelo Ridge ")
+
+# Obtenemos la misma estimación que para Lasso
