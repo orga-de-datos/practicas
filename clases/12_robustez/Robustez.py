953816359f55d1849415b123f30ab26ea1ac93f9
CrossNox <ijmermet@gmail.com>
2021-02-10 04:30:19 -0300

    fix

--- clases/12_robustez/Robustez.py
+++ clases/12_robustez/Robustez.py
@@ -7,11 +7,11 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.5.2
+#       jupytext_version: 1.6.0
 #   kernelspec:
-#     display_name: env3
+#     display_name: Python 3 (venv)
 #     language: python
-#     name: env3
+#     name: python3
 # ---
 
 # +
@@ -27,9 +27,11 @@ sns.set_theme(style="whitegrid")
 
 # # Robustez en regresiones
 
-# ## Outliers vs High Leverage Observations 
+# ## Outliers vs High Leverage Observations
 
-df = pd.read_csv("https://drive.google.com/uc?export=download&id=1yoA3ua0zL6ecG66q5WDchR_iL1sj6v4M")
+df = pd.read_csv(
+    "https://drive.google.com/uc?export=download&id=1yoA3ua0zL6ecG66q5WDchR_iL1sj6v4M"
+)
 
 df.head()
 
@@ -48,7 +50,7 @@ model.fit(df['xx'].values.reshape(-1, 1), df['yy'])
 
 # Calculamos el $R^2$ de la recta:
 
-r2 = model.score(df['xx'].values.reshape(-1, 1),df['yy'])
+r2 = model.score(df['xx'].values.reshape(-1, 1), df['yy'])
 
 # Graficamos la recta obtenida:
 
@@ -59,8 +61,10 @@ plt.plot(df['xx'], pred_linear, color='red')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 plt.title("Regresión lineal", weight="bold", fontsize=18)
-plt.text( 25, 40, "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_), fontsize=16)
-plt.text( 25, 35, r"$R^2$ = {:.3f}".format(r2), fontsize=16)
+plt.text(
+    25, 40, "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_), fontsize=16
+)
+plt.text(25, 35, r"$R^2$ = {:.3f}".format(r2), fontsize=16)
 plt.show()
 
 # Puntos con **alto leverage** son puntos que tienen valores inusuales de los predictores (variables dependientes) o una combinación no usual de predictores(si tenemos más de 1 variable independiente) pero no necesariamente son outliers. En el gráfico de arriba tenemos 2 puntos con alto leverage, y uno es un outlier y otro parece seguir la tendencia de los datos.
@@ -70,34 +74,54 @@ plt.show()
 # +
 # Entreno modelo con punto de alto leverage y sin outlier
 df_leverage = df.copy()
-df_leverage = df_leverage.drop([4,10])
+df_leverage = df_leverage.drop([4, 10])
 model_leverage = LinearRegression()
 model_leverage.fit(df_leverage['xx'].values.reshape(-1, 1), df_leverage['yy'])
-r2_leverage = model_leverage.score(df_leverage['xx'].values.reshape(-1, 1),df_leverage['yy'])
+r2_leverage = model_leverage.score(
+    df_leverage['xx'].values.reshape(-1, 1), df_leverage['yy']
+)
 pred_leverage = model_leverage.predict(df_leverage['xx'].values.reshape(-1, 1))
 
 # Entreno modelo sin puntos de alto leverage y sin outliers
 df_inliers = df.copy()
-df_inliers = df_inliers.drop([4,10,11])
+df_inliers = df_inliers.drop([4, 10, 11])
 model_inliers = LinearRegression()
 model_inliers.fit(df_inliers['xx'].values.reshape(-1, 1), df_inliers['yy'])
-r2_inliers = model_inliers.score(df_inliers['xx'].values.reshape(-1, 1),df_inliers['yy'])
+r2_inliers = model_inliers.score(
+    df_inliers['xx'].values.reshape(-1, 1), df_inliers['yy']
+)
 pred_inliers = model_inliers.predict(df_leverage['xx'].values.reshape(-1, 1))
 
 fig = plt.figure(figsize=(12, 6), dpi=100)
 plt.scatter(df_leverage['xx'], df_leverage['yy'])
 
-plt.plot(df_leverage['xx'], pred_leverage, color='red', label="Con punto de alto leverage")
-plt.plot(df_leverage['xx'], pred_inliers, color='green', label="Sin punto de alto leverage")
+plt.plot(
+    df_leverage['xx'], pred_leverage, color='red', label="Con punto de alto leverage"
+)
+plt.plot(
+    df_leverage['xx'], pred_inliers, color='green', label="Sin punto de alto leverage"
+)
 
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 
-plt.text( 15, 26, "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_), fontsize=16, color="red")
-plt.text( 15, 24, r"$R^2$ = {:.3f}".format(r2_leverage), fontsize=16, color="red")
+plt.text(
+    15,
+    26,
+    "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_),
+    fontsize=16,
+    color="red",
+)
+plt.text(15, 24, r"$R^2$ = {:.3f}".format(r2_leverage), fontsize=16, color="red")
 
-plt.text( 15, 19, "y = {:.2f}x + {:.2f}".format(model_inliers.coef_[0], model_inliers.intercept_), fontsize=16, color="green")
-plt.text( 15, 17, r"$R^2$ = {:.3f}".format(r2_inliers), fontsize=16, color="green") 
+plt.text(
+    15,
+    19,
+    "y = {:.2f}x + {:.2f}".format(model_inliers.coef_[0], model_inliers.intercept_),
+    fontsize=16,
+    color="green",
+)
+plt.text(15, 17, r"$R^2$ = {:.3f}".format(r2_inliers), fontsize=16, color="green")
 plt.title("Alto leverage != outlier", weight="bold", fontsize=18)
 plt.legend(loc=2, fontsize=14, title="Regresión").get_title().set_fontsize(15)
 plt.show()
@@ -115,8 +139,10 @@ plt.plot(df['xx'], pred_linear, color='red')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 plt.title("Regresión lineal", weight="bold", fontsize=18)
-plt.text( 25, 40, "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_), fontsize=16)
-plt.text( 25, 35, r"$R^2$ = {:.3f}".format(r2), fontsize=16)
+plt.text(
+    25, 40, "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_), fontsize=16
+)
+plt.text(25, 35, r"$R^2$ = {:.3f}".format(r2), fontsize=16)
 plt.show()
 # -
 
@@ -125,7 +151,7 @@ plt.show()
 from sklearn.linear_model import HuberRegressor
 from sklearn.metrics import r2_score
 
-huber = HuberRegressor( epsilon=1.35).fit(df['xx'].values.reshape(-1, 1), df['yy'])
+huber = HuberRegressor(epsilon=1.35).fit(df['xx'].values.reshape(-1, 1), df['yy'])
 
 # +
 pred_huber = huber.predict(df['xx'].values.reshape(-1, 1))
@@ -141,26 +167,50 @@ plt.plot(df['xx'], pred_huber, color='orange', label="Huber regression")
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 plt.title("Regresión lineal con Huber loss $\epsilon$=1.35", weight="bold", fontsize=18)
-plt.text( 25, 38, "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_), fontsize=16, color="green")
-plt.text( 25, 41, "y = {:.2f}x + {:.2f}".format(huber.coef_[0], huber.intercept_), fontsize=16, color="orange")
-plt.text( 25, 44, "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_), fontsize=16, color="red")
+plt.text(
+    25,
+    38,
+    "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_),
+    fontsize=16,
+    color="green",
+)
+plt.text(
+    25,
+    41,
+    "y = {:.2f}x + {:.2f}".format(huber.coef_[0], huber.intercept_),
+    fontsize=16,
+    color="orange",
+)
+plt.text(
+    25,
+    44,
+    "y = {:.2f}x + {:.2f}".format(model.coef_[0], model.intercept_),
+    fontsize=16,
+    color="red",
+)
 
-plt.legend(loc=(0.7,0.3), fontsize=14, title="Regresión").get_title().set_fontsize(15)
+plt.legend(loc=(0.7, 0.3), fontsize=14, title="Regresión").get_title().set_fontsize(15)
 plt.show()
 # -
 
-# La recta esta mucho mejor ajustada, aunque todavía no es la ideal. Esto es debido a que la función de huber le resta importancia a los outliers pero no los elimina por completo. 
+# La recta esta mucho mejor ajustada, aunque todavía no es la ideal. Esto es debido a que la función de huber le resta importancia a los outliers pero no los elimina por completo.
 #
 # Que pasa si aumentamos el valor de epsilon?
 
 # +
-huber_eps_1_35 = HuberRegressor(alpha=0.01, epsilon=1.35).fit(df['xx'].values.reshape(-1, 1), df['yy'])
+huber_eps_1_35 = HuberRegressor(alpha=0.01, epsilon=1.35).fit(
+    df['xx'].values.reshape(-1, 1), df['yy']
+)
 pred_huber_eps_1_35 = huber_eps_1_35.predict(df['xx'].values.reshape(-1, 1))
 
-huber_eps_1_5= HuberRegressor(alpha=0.01, epsilon=1.5).fit(df['xx'].values.reshape(-1, 1), df['yy'])
+huber_eps_1_5 = HuberRegressor(alpha=0.01, epsilon=1.5).fit(
+    df['xx'].values.reshape(-1, 1), df['yy']
+)
 pred_huber_eps_1_5 = huber_eps_1_5.predict(df['xx'].values.reshape(-1, 1))
 
-huber_eps_1_7 = HuberRegressor(alpha=0.01, epsilon=1.7).fit(df['xx'].values.reshape(-1, 1), df['yy'])
+huber_eps_1_7 = HuberRegressor(alpha=0.01, epsilon=1.7).fit(
+    df['xx'].values.reshape(-1, 1), df['yy']
+)
 pred_huber_eps_1_7 = huber_eps_1_7.predict(df['xx'].values.reshape(-1, 1))
 
 
@@ -176,9 +226,21 @@ plt.plot(df['xx'], pred_linear, color='lime', label="linear regression")
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 plt.title(r"Regresión lineal con Huber loss $\epsilon$=20", weight="bold", fontsize=18)
-plt.text( 25, 41, "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_), fontsize=16, color="red")
-plt.text( 25, 38, "y = {:.2f}x + {:.2f}".format(huber.coef_[0], huber.intercept_), fontsize=16, color="orange")
-plt.legend(loc=(0.7,0.4), fontsize=14, title="Regresión").get_title().set_fontsize(15)
+plt.text(
+    25,
+    41,
+    "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_),
+    fontsize=16,
+    color="red",
+)
+plt.text(
+    25,
+    38,
+    "y = {:.2f}x + {:.2f}".format(huber.coef_[0], huber.intercept_),
+    fontsize=16,
+    color="orange",
+)
+plt.legend(loc=(0.7, 0.4), fontsize=14, title="Regresión").get_title().set_fontsize(15)
 
 plt.show()
 # -
@@ -187,7 +249,9 @@ plt.show()
 
 from sklearn.linear_model import RANSACRegressor
 
-RANSAC_model = RANSACRegressor(residual_threshold=15).fit(df['xx'].values.reshape(-1, 1), df['yy'])
+RANSAC_model = RANSACRegressor(residual_threshold=15).fit(
+    df['xx'].values.reshape(-1, 1), df['yy']
+)
 
 # +
 pred_ransac = RANSAC_model.predict(df['xx'].values.reshape(-1, 1))
@@ -197,15 +261,29 @@ plt.plot(df['xx'], pred_ransac, color='olive', label="RANSAC")
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 plt.plot(df_leverage['xx'], pred_leverage, color='red', label="Sin outliers")
-plt.text( 15, 35, "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_), fontsize=16, color="red")
-plt.text( 15, 30, "y = {:.2f}x + {:.2f}".format(RANSAC_model.estimator_.coef_[0], RANSAC_model.estimator_.intercept_), fontsize=16, color="olive")
+plt.text(
+    15,
+    35,
+    "y = {:.2f}x + {:.2f}".format(model_leverage.coef_[0], model_leverage.intercept_),
+    fontsize=16,
+    color="red",
+)
+plt.text(
+    15,
+    30,
+    "y = {:.2f}x + {:.2f}".format(
+        RANSAC_model.estimator_.coef_[0], RANSAC_model.estimator_.intercept_
+    ),
+    fontsize=16,
+    color="olive",
+)
 plt.title("Regresión RANSAC", weight="bold", fontsize=18)
-plt.legend(loc=(0.7,0.4), fontsize=14, title="Regresión").get_title().set_fontsize(15)
+plt.legend(loc=(0.7, 0.4), fontsize=14, title="Regresión").get_title().set_fontsize(15)
 
 plt.show()
 # -
 
-# La regresión obtenida es igual a la obtenida sin tener en cuenta los puntos outliers en este caso (caso particular). Sin embargo es "mejor" que la regresión lineal y que Huber. 
+# La regresión obtenida es igual a la obtenida sin tener en cuenta los puntos outliers en este caso (caso particular). Sin embargo es "mejor" que la regresión lineal y que Huber.
 #
 # Hasta cuantos Outliers soporta RANSAC?
 
@@ -214,9 +292,14 @@ n_samples = 1000
 n_outliers = 50
 
 
-X, y, coef = datasets.make_regression(n_samples=n_samples, n_features=1,
-                                      n_informative=1, noise=10,
-                                      coef=True, random_state=0)
+X, y, coef = datasets.make_regression(
+    n_samples=n_samples,
+    n_features=1,
+    n_informative=1,
+    noise=10,
+    coef=True,
+    random_state=0,
+)
 
 # Add outlier data
 np.random.seed(0)
@@ -250,19 +333,34 @@ fig = plt.figure(figsize=(12, 6), dpi=100)
 
 plt.scatter(X, y, marker='.')
 plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Lineal')
-plt.plot(line_X, line_y_ransac, color='tomato', linewidth=lw,
-         label='RANSAC')
-plt.legend(loc='lower right', title="Regresión", fontsize=14).get_title().set_fontsize(15)
+plt.plot(line_X, line_y_ransac, color='tomato', linewidth=lw, label='RANSAC')
+plt.legend(loc='lower right', title="Regresión", fontsize=14).get_title().set_fontsize(
+    15
+)
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
-plt.text(-1, 150, "y = {:.2f}x + {:.2f}".format(ransac.estimator_.coef_[0], ransac.estimator_.intercept_), fontsize=16, color="tomato")
-plt.text(-1, 200, "y = {:.2f}x + {:.2f}".format(lr.coef_[0], lr.intercept_), fontsize=16, color="navy")
+plt.text(
+    -1,
+    150,
+    "y = {:.2f}x + {:.2f}".format(
+        ransac.estimator_.coef_[0], ransac.estimator_.intercept_
+    ),
+    fontsize=16,
+    color="tomato",
+)
+plt.text(
+    -1,
+    200,
+    "y = {:.2f}x + {:.2f}".format(lr.coef_[0], lr.intercept_),
+    fontsize=16,
+    color="navy",
+)
 
 plt.title("Regresión RANSAC y regresión lineal ordinaria", weight="bold", fontsize=18)
 plt.show()
 # -
 
-from sklearn.linear_model  import TheilSenRegressor
+from sklearn.linear_model import TheilSenRegressor
 
 # +
 lr.fit(X, y)
@@ -285,11 +383,35 @@ plt.plot(line_X, line_y_ransac, color='tomato', linewidth=lw, label='RANSAC')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
 
-plt.text(-1, 300, "y = {:.2f}x + {:.2f}".format(lr.coef_[0], lr.intercept_), fontsize=16, color="navy")
-plt.text(-1, 250, "y = {:.2f}x + {:.2f}".format(theil_model.coef_[0], theil_model.intercept_), fontsize=16, color="green")
-plt.text(-1, 200, "y = {:.2f}x + {:.2f}".format(ransac.estimator_.coef_[0], ransac.estimator_.intercept_), fontsize=16, color="tomato")
-plt.title("Regresión Theil Sen y regresión lineal ordinaria", weight="bold", fontsize=18)
-plt.legend(loc='lower right', title="Regresión", fontsize=14).get_title().set_fontsize(15)
+plt.text(
+    -1,
+    300,
+    "y = {:.2f}x + {:.2f}".format(lr.coef_[0], lr.intercept_),
+    fontsize=16,
+    color="navy",
+)
+plt.text(
+    -1,
+    250,
+    "y = {:.2f}x + {:.2f}".format(theil_model.coef_[0], theil_model.intercept_),
+    fontsize=16,
+    color="green",
+)
+plt.text(
+    -1,
+    200,
+    "y = {:.2f}x + {:.2f}".format(
+        ransac.estimator_.coef_[0], ransac.estimator_.intercept_
+    ),
+    fontsize=16,
+    color="tomato",
+)
+plt.title(
+    "Regresión Theil Sen y regresión lineal ordinaria", weight="bold", fontsize=18
+)
+plt.legend(loc='lower right', title="Regresión", fontsize=14).get_title().set_fontsize(
+    15
+)
 
 plt.show()
 # -
@@ -303,9 +425,14 @@ n_samples = 1000
 n_outliers = 410
 
 
-X, y, coef = datasets.make_regression(n_samples=n_samples, n_features=1,
-                                      n_informative=1, noise=10,
-                                      coef=True, random_state=0)
+X, y, coef = datasets.make_regression(
+    n_samples=n_samples,
+    n_features=1,
+    n_informative=1,
+    noise=10,
+    coef=True,
+    random_state=0,
+)
 
 # Add outlier data
 np.random.seed(0)
@@ -323,7 +450,7 @@ ransac.fit(X, y)
 theil_model = TheilSenRegressor(random_state=42).fit(X, y)
 
 
-#Predict data of estimated models
+# Predict data of estimated models
 line_X = np.arange(X.min(), X.max())[:, np.newaxis]
 line_y = lr.predict(line_X)
 line_y_ransac = ransac.predict(line_X)
@@ -331,12 +458,16 @@ line_y_theil = theil_model.predict(line_X)
 
 fig = plt.figure(figsize=(12, 6), dpi=100)
 
-plt.scatter(X, y,marker='.')
+plt.scatter(X, y, marker='.')
 plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=lw,
-         label='RANSAC regressor')
-plt.plot(line_X, line_y_theil, color='green', linewidth=lw,
-         label='Theil Sen regressor')
+plt.plot(
+    line_X,
+    line_y_ransac,
+    color='cornflowerblue',
+    linewidth=lw,
+    label='RANSAC regressor',
+)
+plt.plot(line_X, line_y_theil, color='green', linewidth=lw, label='Theil Sen regressor')
 plt.legend(loc='upper right')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
@@ -346,21 +477,29 @@ plt.show()
 
 # Puedo usar cualquier estimador dentro de RANSAC:
 
-ransac_theil = linear_model.RANSACRegressor(base_estimator=TheilSenRegressor(random_state=42), random_state=1)
+ransac_theil = linear_model.RANSACRegressor(
+    base_estimator=TheilSenRegressor(random_state=42), random_state=1
+)
 ransac_theil.fit(X, y)
 line_y_ransac_theil = ransac_theil.predict(line_X)
 
 # +
 fig = plt.figure(figsize=(12, 6), dpi=100)
 
-plt.plot(line_X, line_y_ransac_theil, color='orange', linewidth=lw, label='RANSAC(Theil Sen)')
-plt.plot(line_X, line_y_theil, color='green', linewidth=lw,
-         label='Theil Sen regressor')
+plt.plot(
+    line_X, line_y_ransac_theil, color='orange', linewidth=lw, label='RANSAC(Theil Sen)'
+)
+plt.plot(line_X, line_y_theil, color='green', linewidth=lw, label='Theil Sen regressor')
 plt.plot(line_X, line_y, color='navy', linewidth=lw, label='Linear regressor')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', linewidth=lw,
-         label='RANSAC(linear) regressor')
+plt.plot(
+    line_X,
+    line_y_ransac,
+    color='cornflowerblue',
+    linewidth=lw,
+    label='RANSAC(linear) regressor',
+)
 
-plt.scatter(X, y,marker='.')
+plt.scatter(X, y, marker='.')
 plt.legend(loc='upper right')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
@@ -371,13 +510,17 @@ plt.show()
 # Cuidado con métodos iterativos con inicialización
 
 # +
-ransac_theil = linear_model.RANSACRegressor(base_estimator=TheilSenRegressor(random_state=42), random_state=42)
+ransac_theil = linear_model.RANSACRegressor(
+    base_estimator=TheilSenRegressor(random_state=42), random_state=42
+)
 ransac_theil.fit(X, y)
 line_y_ransac_theil = ransac_theil.predict(line_X)
 fig = plt.figure(figsize=(12, 6), dpi=100)
 
-plt.plot(line_X, line_y_ransac_theil, color='navy', linewidth=lw, label='RANSAC(Theil Sen)')
-plt.scatter(X, y,marker='.')
+plt.plot(
+    line_X, line_y_ransac_theil, color='navy', linewidth=lw, label='RANSAC(Theil Sen)'
+)
+plt.scatter(X, y, marker='.')
 plt.legend(loc='lower right')
 plt.xlabel('X', weight="bold", fontsize=16)
 plt.ylabel('Y', weight="bold", fontsize=16)
@@ -455,7 +598,7 @@ ax[0, 0].set_title("Pequeños outliers en Y", weight="bold")
 ax[0, 0].set_ylabel("y", weight="bold")
 
 ax[0, 1].scatter(X_errors, y)
-ax[0, 1].set_title("Pequeños outliers en X",weight="bold")
+ax[0, 1].set_title("Pequeños outliers en X", weight="bold")
 
 
 ax[1, 0].scatter(X, y_errors_large)
@@ -542,5 +685,3 @@ plt.show()
 # A pesar de un rendimiento superior sobre los modelos de mínimos cuadrados, los modelos robustos todavía no se utilizan ampliamente. Las razones son:
 # La regresión robusta requiere cálculos pesados. A veces, los valores atípicos son un requisito de un modelo y no es facil determinar qué es un valor atípico, sobretodo en datos de alta dimensión. *The End*
 #
-
-
